5f2da2c7b71904bf598ae58cb220e3df7f4ae2a6
Gireesh-pds <gireesh.kumar@privacydatasystems.com>
2024-01-18 14:25:02 +0530

    Weather-app responsive updated

--- src/App.js
+++ src/App.js
@@ -51,6 +51,7 @@ function App() {
         }
         setWeatherForcast(forcastData);
         setWeatherForcastLoader(false);
+        alert('Data fetched Successfully !')
       })
       .catch((err) => {
         setWeatherForcastLoader(false);
